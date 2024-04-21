dd0d2f27d9802b011551090e56df91c43ca87720
Naman Jain <naman1205jain@gmail.com>
2024-04-20 18:51:21 -0700

    fix selfrepair

--- lcb_runner/runner/base_runner.py
+++ lcb_runner/runner/base_runner.py
@@ -96,6 +96,21 @@ class BaseRunner(ABC):
 
         return outputs
 
+    def prompts_to_outputs(
+        self, prompts: list[str | list[dict[str, str]]]
+    ) -> list[list[str]]:
+        if self.args.use_cache:
+            outputs = []
+            batch_size = self.args.cache_batch_size
+            for i in range(0, len(prompts), batch_size):
+                batch = prompts[i : i + batch_size]
+                batch_outputs = self.run_batch(batch)
+                outputs.extend(batch_outputs)
+            self.save_cache()
+        else:
+            outputs = self.run_batch(prompts)
+        return outputs
+
     def run_main_repair(self, format_prompt: callable) -> list[list[str]]:
         assert self.args.n == 1
         with open(
@@ -104,7 +119,8 @@ class BaseRunner(ABC):
             check_metadata_list = json.load(f)
 
         outputs = [
-            [None for _ in range(self.args.n)] for _ in range(len(check_metadata_list))
+            [None for _ in range(self.args.codegen_n)]
+            for _ in range(len(check_metadata_list))
         ]
         prompts = []
         prompt_index_to_question_idx = {}
@@ -131,7 +147,7 @@ class BaseRunner(ABC):
                 prompt_index_to_question_idx[len(prompts) - 1] = check_metadata_idx
                 prompt_index_to_code_idx[len(prompts) - 1] = code_idx
 
-        prompt_outputs = self.run_batch(prompts)
+        prompt_outputs = self.prompts_to_outputs(prompts)
         for prompt_idx, output in enumerate(prompt_outputs):
             question_idx = prompt_index_to_question_idx[prompt_idx]
             code_idx = prompt_index_to_code_idx[prompt_idx]
@@ -146,14 +162,5 @@ class BaseRunner(ABC):
         prompts = [
             format_prompt(problem, self.model.model_style) for problem in benchmark
         ]
-        if self.args.use_cache:
-            outputs = []
-            batch_size = self.args.cache_batch_size
-            for i in range(0, len(prompts), batch_size):
-                batch = prompts[i : i + batch_size]
-                batch_outputs = self.run_batch(batch)
-                outputs.extend(batch_outputs)
-            self.save_cache()
-        else:
-            outputs = self.run_batch(prompts)
+        outputs = self.prompts_to_outputs(prompts)
         return outputs
