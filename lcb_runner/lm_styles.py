92f9cf26ff07cff48e958dd35f3b51f885e35ff0
Thomas YIu <tom.tyiu@gmail.com>
2025-07-24 22:34:51 -0700

    Update lm_styles.py

--- lcb_runner/lm_styles.py
+++ lcb_runner/lm_styles.py
@@ -861,6 +861,13 @@ LanguageModelList: list[LanguageModel] = [
         datetime(2024, 4, 1),
         "https://huggingface.co/agentica-org/DeepCoder-14B-Preview",
     ),
+    LanguageModel(
+        "EpistemeAI/Hercules-Coder-E4B-it",
+        "Hercules-Coder-E4B-it",
+        LMStyle.GenericBase,
+        datetime(2025, 7, 24),
+        link="https://huggingface.co/EpistemeAI/Hercules-Coder-E4B-it",
+    ),
 ]
 
 LanguageModelStore: dict[str, LanguageModel] = {
